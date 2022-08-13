0fab942e8bc3763d44db37e8c3be7d8cd5c2fd57
Zach Sisco <zach@zsisco.net>
2022-08-12 22:32:52 -0700

    first commit

---
+++ pipelined-alu-2stg.py
@@ -0,0 +1,113 @@
+import pyrtl
+from enum import IntEnum
+
+class ALUOp(IntEnum):
+    ADD = 0x0
+    SUB = 0x1
+    XOR = 0x2
+    AND = 0x3
+    OR  = 0x4
+
+# Declarations:
+# -------------
+
+# Stall - if high, propagate NOP through pipeline
+stall = pyrtl.Input(1, 'stall')
+# "Fetch" - input to design each step is a 16-bit instruction
+# If stall is high, instruction input is ignored
+inst = pyrtl.Input(16, 'inst')
+
+# Register file is a memory mapping 4-bit address to 16-bit data value
+rf = pyrtl.MemBlock(bitwidth=16, addrwidth=4, name='rf')
+
+# ALU registers for inputs and result
+in1 = pyrtl.Register(16, 'in1')
+in2 = pyrtl.Register(16, 'in2')
+alu_result = pyrtl.WireVector(16, 'alu_out')
+
+# Pipeline registers
+c_addr_1 = pyrtl.Register(4, 'c_addr_1')
+op_1 = pyrtl.Register(4, 'op_1')
+write_1 = pyrtl.Register(1, 'write_1')
+
+# Stage 0:
+# -------------------
+
+# Decode instruction:
+# opcode, two source registers (a, b), destination register (c)
+inst_a = inst[0:4]
+inst_b = inst[4:8]
+inst_c = inst[8:12]
+inst_op = inst[12:16]
+
+# In next cycle, op_1 gets updated with inst_op
+op_1.next <<= inst_op
+
+# Read from register file and do register forwarding
+with pyrtl.conditional_assignment:
+    with stall:
+        in1.next |= 0
+    with c_addr_1 == inst_a:
+        in1.next |= alu_result
+    with pyrtl.otherwise:
+        in1.next |= rf[inst_a]
+
+with pyrtl.conditional_assignment:
+    with stall:
+        in2.next |= 0
+    with c_addr_1 == inst_b:
+        in2.next |= alu_result
+    with pyrtl.otherwise:
+        in2.next |= rf[inst_b]
+
+with pyrtl.conditional_assignment:
+    with stall:
+        write_1.next |= 0
+        c_addr_1.next |= 0
+    with pyrtl.otherwise:
+        write_1.next |= 1
+        c_addr_1.next |= inst_c
+
+# Stage 1:
+# -------------------
+# Perform ALU operation
+alu_result <<= pyrtl.enum_mux(
+        op_1,
+        {
+            ALUOp.ADD: in1 + in2,
+            ALUOp.SUB: in1 - in2,
+            ALUOp.XOR: in1 ^ in2,
+            ALUOp.OR: in1 | in2,
+            ALUOp.AND: in1 & in2,
+        },
+        default=0)
+
+# Write result back to register file
+rf[c_addr_1] <<= pyrtl.MemBlock.EnabledWrite(alu_result, write_1)
+
+# ------------------------------------------------------------------------------
+
+# Start a simulation
+sim_trace = pyrtl.SimulationTrace()
+
+# Initialize reg file
+rf_init = {}
+for i in range(16):
+    rf_init[i] = i
+
+sim = pyrtl.Simulation(tracer=sim_trace, memory_value_map={
+    rf : rf_init,
+})
+
+# Run
+sim.step({'inst': 0x0112, 'stall': 0})
+sim.step({'inst': 0x0333, 'stall': 0})
+sim.step({'inst': 0x1221, 'stall': 0})
+sim.step({'inst': 0x1121, 'stall': 0})
+sim.step({'inst': 0x0000, 'stall': 1})
+sim.step({'inst': 0x0000, 'stall': 1})
+
+sim_trace.render_trace(symbol_len=10, segment_size=1)
+
+# Print out the register file
+print(sim.inspect_mem(rf))
---
+++ pipelined-alu-3stg-pc.py
@@ -0,0 +1,212 @@
+import pyrtl
+from enum import IntEnum
+
+"""
+Register instructions:
+op rd rs1 rs2 : rd <- (op rs1 rs2)
+  with op in (ADD|SUB|XOR|AND|OR)
+
+Unconditional branch:
+JUMP immediate : pc <- (add pc immediate)
+"""
+
+WIDTH = 16
+
+class OP(IntEnum):
+    NOP  = 0x0
+    ADD  = 0x1
+    SUB  = 0x2
+    XOR  = 0x3
+    AND  = 0x4
+    OR   = 0x5
+    JUMP = 0x6
+
+# Declarations:
+# -------------
+
+# Stall input - if high, propagate NOP through pipeline
+stall = pyrtl.Input(1, 'stall')
+
+# Program counter
+pc = pyrtl.Register(WIDTH, 'pc')
+
+# Instruction memory
+imem = pyrtl.MemBlock(bitwidth=WIDTH, addrwidth=WIDTH, name='imem')
+
+# Register file is a memory mapping 4-bit address to 16-bit data value
+# Zero register is reserved 0 value
+rf = pyrtl.MemBlock(bitwidth=WIDTH, addrwidth=4, name='rf', asynchronous=True)
+
+# Pipeline registers:
+#   `d_` and `x_` indicate which stage the registers are used
+#   (`d` for decode, `x` for execute)
+d_inst = pyrtl.Register(WIDTH, 'd_inst')
+d_pc = pyrtl.Register(WIDTH, 'pc_d')
+x_rd = pyrtl.Register(4, 'x_rd')
+x_ctrl_alu_op = pyrtl.Register(4, 'x_ctrl_alu_op')
+x_ctrl_branch = pyrtl.Register(1, 'x_ctrl_branch')
+x_ctrl_write = pyrtl.Register(1, 'x_ctrl_write')
+x_alu_in1 = pyrtl.Register(WIDTH, 'x_alu_in1')
+x_alu_in2 = pyrtl.Register(WIDTH, 'x_alu_in2')
+
+# Wire to hold ALU result
+alu_result = pyrtl.WireVector(WIDTH, 'alu_out')
+
+# Stage 1: Fetch
+# --------------
+# Read from instruction memory at current PC
+# Store fetched instruction in instruction register
+# Will be available in the *next* cycle
+d_inst.next <<= imem[pc]
+
+branch_taken = pyrtl.WireVector(1, 'branch_taken')
+branch_target = pyrtl.WireVector(WIDTH, 'branch_target')
+
+# Increment PC
+# `|=` is PyRTL's "conditional assignment" operator, used in `with` expressions
+with pyrtl.conditional_assignment:
+    with stall:
+        pc.next |= pc
+    with branch_taken:
+        pc.next |= branch_target
+    with pyrtl.otherwise:
+        pc.next |= pc + 1
+
+d_pc.next <<= pc
+
+# Stage 2: Decode
+# ---------------
+nop = pyrtl.WireVector(1, 'nop')
+nop <<= branch_taken | stall
+
+# Intermediate wires for op, 2 source registers (a, b), destination register (c)
+# A NOP instruction is ADD r0 r0 r0
+inst_a = pyrtl.mux(nop, d_inst[0:4], pyrtl.Const(0, bitwidth=4))
+inst_b = pyrtl.mux(nop, d_inst[4:8], pyrtl.Const(0, bitwidth=4))
+inst_c = pyrtl.mux(nop, d_inst[8:12], pyrtl.Const(0, bitwidth=4))
+inst_op = pyrtl.mux(nop, d_inst[12:16], OP.ADD)
+inst_imm = d_inst[0:12].zero_extended(WIDTH)
+
+# Control signals
+ctrl_branch = pyrtl.WireVector(1, 'ctrl_branch')
+ctrl_alu_op = pyrtl.WireVector(4, 'ctrl_alu_op')
+ctrl_write = pyrtl.WireVector(1, 'ctrl_write')
+
+# Control logic
+#ctrl_alu_op <<= pyrtl.net_hole('hole_ctrl_alu_op', 4, inst_op)
+ctrl_alu_op <<= pyrtl.enum_mux(
+        inst_op,
+        {
+            OP.JUMP: OP.ADD,
+        },
+        default=inst_op)
+
+#ctrl_branch <<= pyrtl.net_hole('hole_ctrl_branch', 1, inst_op)
+ctrl_branch <<= pyrtl.enum_mux(
+        inst_op,
+        {
+            OP.JUMP: 1,
+        },
+        default=0)
+
+#ctrl_write <<= pyrtl.net_hole('hole_ctrl_write', 1, inst_op)
+ctrl_write <<= pyrtl.enum_mux(
+        inst_op,
+        {
+            OP.ADD: 1,
+            OP.SUB: 1,
+            OP.XOR: 1,
+            OP.OR:  1,
+            OP.AND: 1,
+        },
+        default=0)
+
+# Read from register file and do register forwarding
+# ALU input 1:
+with pyrtl.conditional_assignment:
+    with ctrl_branch:
+        x_alu_in1.next |= d_pc
+    with (x_rd == inst_a) & (inst_a != 0):
+        x_alu_in1.next |= alu_result
+    with pyrtl.otherwise:
+        x_alu_in1.next |= rf[inst_a]
+
+# ALU input 2:
+with pyrtl.conditional_assignment:
+    with ctrl_branch:
+        x_alu_in2.next |= inst_imm
+    with (x_rd == inst_b) & (inst_b != 0):
+        x_alu_in2.next |= alu_result
+    with pyrtl.otherwise:
+        x_alu_in2.next |= rf[inst_b]
+
+# Forward control signals to next stage
+x_ctrl_alu_op.next <<= ctrl_alu_op
+x_ctrl_branch.next <<= ctrl_branch
+x_ctrl_write.next <<= ctrl_write
+x_rd.next <<= inst_c
+
+# Stage 3: Execute and write back
+# -------------------------------
+# Perform ALU operation
+alu_result <<= pyrtl.enum_mux(
+        x_ctrl_alu_op,
+        {
+            OP.ADD: x_alu_in1 + x_alu_in2,
+            OP.SUB: x_alu_in1 - x_alu_in2,
+            OP.XOR: x_alu_in1 ^ x_alu_in2,
+            OP.OR:  x_alu_in1 | x_alu_in2,
+            OP.AND: x_alu_in1 & x_alu_in2,
+        },
+        default=0)
+
+branch_taken <<= x_ctrl_branch
+branch_target <<= alu_result
+
+# Write result back to register file
+reg_write_enable = pyrtl.WireVector(1, 'reg_write_enable')
+reg_write_enable <<= x_ctrl_write & (x_rd != 0)
+rf[x_rd] <<= pyrtl.MemBlock.EnabledWrite(alu_result, reg_write_enable)
+
+# -------------------------------------------------------------------------------
+
+# Start a simulation
+sim_trace = pyrtl.SimulationTrace()
+
+# Initialize reg file
+rf_init = {}
+for i in range(16):
+    if i in range(4):
+        rf_init[i] = i
+    else:
+        rf_init[i] = 0
+
+imem_init = {
+    0: 0x1112,
+    1: 0x1333,
+    2: 0x6007,
+    3: 0x2221,
+    4: 0x1000,
+    9: 0x2121,
+}
+
+sim = pyrtl.Simulation(tracer=sim_trace, memory_value_map={
+    rf : rf_init,
+    imem : imem_init,
+})
+
+# Run
+sim.step({'stall': 0})
+sim.step({'stall': 0})
+sim.step({'stall': 0})
+sim.step({'stall': 0})
+sim.step({'stall': 0})
+sim.step({'stall': 0})
+sim.step({'stall': 0})
+sim.step({'stall': 1})
+sim.step({'stall': 1})
+
+sim_trace.render_trace(symbol_len=10, segment_size=1)
+
+# Print out the register file
+print(sim.inspect_mem(rf))
